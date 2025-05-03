01fc6f626b4816a8233db794311cfe22d5d76a73
Andreas Kugel <info@digital-codes.de>
2025-05-03 10:28:21 +0200

    docling test update. looks nice

--- langchain/testdocling.py
+++ langchain/testdocling.py
@@ -10,26 +10,40 @@ from docling.datamodel.pipeline_options import (
 )
 from docling.document_converter import DocumentConverter, PdfFormatOption
 
-
-source = "https://arxiv.org/pdf/2408.09869"  # document per local path or URL
+import sys
+if len(sys.argv) > 1:
+    source = sys.argv[1]  # document per local path or URL
+else:
+    source = "https://arxiv.org/pdf/2408.09869"  # document per local path or URL
 
 # Set lang=["auto"] with a tesseract OCR engine: TesseractOcrOptions, TesseractCliOcrOptions
 # ocr_options = TesseractOcrOptions(lang=["auto"])
 ocr_options = TesseractCliOcrOptions(lang=["auto"])
 
-pipeline_options = PdfPipelineOptions(
-    do_ocr=True, force_full_page_ocr=True, ocr_options=ocr_options
-)
+IMAGE_RESOLUTION_SCALE = 2.0
+
 if USE_OCR:
-    converter = DocumentConverter(
-        format_options={
-            InputFormat.PDF: PdfFormatOption(
-                pipeline_options=pipeline_options,
-            )
-        }
+    pipeline_options = PdfPipelineOptions(
+        do_ocr=True,
+        force_full_page_ocr=True,
+        ocr_options=ocr_options
     )
 else:
-    converter = DocumentConverter()
+    pipeline_options = PdfPipelineOptions()
+
+# defaults
+pipeline_options.images_scale = IMAGE_RESOLUTION_SCALE
+pipeline_options.generate_page_images = True
+pipeline_options.generate_picture_images = True
+
+
+converter = DocumentConverter(
+    format_options={
+        InputFormat.PDF: PdfFormatOption(
+            pipeline_options=pipeline_options,
+        )
+    }
+)
 
 result = converter.convert(source)
 print(
---
+++ projects/mobility/exportTables.py
@@ -0,0 +1,77 @@
+import os
+import pandas as pd
+from pathlib import Path
+import sys
+import time
+from docling.datamodel import document
+from docling_core.types.doc import ImageRefMode, PictureItem, TableItem, ProvenanceItem
+
+if len(sys.argv) > 1:
+    input_file = sys.argv[1]
+else:
+    input_file = "docling_test_raw.json"
+
+doc =  document.DoclingDocument.load_from_json(input_file)
+# results in document already 
+
+IMAGE_RESOLUTION_SCALE = 2.0
+
+
+start_time = time.time()
+
+output_dir = Path("scratch")
+output_dir.mkdir(parents=True, exist_ok=True)
+
+# Export tables
+for table_ix, table in enumerate(doc.tables):
+    table_df: pd.DataFrame = table.export_to_dataframe()
+    print(f"## Table {table_ix}")
+    print(table_df.to_markdown())
+
+    # Save the table as csv
+    element_csv_filename = output_dir / f"{input_file}-table-{table_ix + 1}.csv"
+    print(f"Saving CSV table to {element_csv_filename}")
+    table_df.to_csv(element_csv_filename)
+
+    # Save the table as html
+    element_html_filename = output_dir / f"{input_file}-table-{table_ix + 1}.html"
+    print(f"Saving HTML table to {element_html_filename}")
+    with element_html_filename.open("w") as fp:
+        fp.write(table.export_to_html(doc=doc))
+
+# Save page images
+for page_no, page in doc.pages.items():
+    page_no = page.page_no
+    page_image_filename = output_dir / f"{input_file}-{page_no}.png"
+    if page.image is not None:
+        with page_image_filename.open("wb") as fp:
+            page.image.pil_image.save(fp, format="PNG")
+
+# Save images of figures and tables
+table_counter = 0
+picture_counter = 0
+for element, _level in doc.iterate_items():
+    if isinstance(element, TableItem):
+        img = element.get_image(doc)
+        if img == None:
+            print("Invalid table image on page",element.prov[0].page_no)
+            continue            
+        table_counter += 1
+        element_image_filename = (
+            output_dir / f"{input_file}-table-{table_counter}.png"
+        )
+        with element_image_filename.open("wb") as fp:
+            img.save(fp, "PNG")
+
+    if isinstance(element, PictureItem):
+        img = element.get_image(doc)
+        if img == None:
+            print("Invaild picture on page",element.prov[0].page_no)
+            continue            
+        picture_counter += 1
+        element_image_filename = (
+            output_dir / f"{input_file}-picture-{picture_counter}.png"
+        )
+        with element_image_filename.open("wb") as fp:
+            img.save(fp, "PNG")
+
