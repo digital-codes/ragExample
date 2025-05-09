d28cf8fc336ac7adfc55f2f37e873b27577a3dbe
Andreas Kugel <info@digital-codes.de>
2025-05-09 13:53:44 +0200

    services machine dependent path

--- sv/run/start_llama_embed.sh
+++ sv/run/start_llama_embed.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-source /opt/oneapi/setvars.sh
-export LD_LIBRARY_PATH="/opt/llama/gpu/lib64:$LD_LIBRARY_PATH"
-export PATH="/opt/llama/gpu/bin:$PATH"
+hostname=$(hostname)
+
+if [[ "$hostname" == "lap3" ]]; then
+    export LD_LIBRARY_PATH="/opt/llama/lib:$LD_LIBRARY_PATH"
+    export PATH="/opt/llama/bin:$PATH"
+elif [[ "$hostname" == "tux" ]]; then
+    source /opt/oneapi/setvars.sh
+    export LD_LIBRARY_PATH="/opt/llama/gpu/lib64:$LD_LIBRARY_PATH"
+    export PATH="/opt/llama/gpu/bin:$PATH"
+else
+    echo "Unknown hostname. Exiting."
+    exit 1
+fi
 exec llama-server -m /opt/llama/models/bge-m3-Q4_K_M.gguf --embeddings --port 8085 --no-webui
--- sv/run/start_llama_jinja.sh
+++ sv/run/start_llama_jinja.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-source /opt/oneapi/setvars.sh
-export LD_LIBRARY_PATH="/opt/llama/gpu/lib64:$LD_LIBRARY_PATH"
-export PATH="/opt/llama/gpu/bin:$PATH"
+hostname=$(hostname)
+if [[ "$hostname" == "lap3" ]]; then
+    export LD_LIBRARY_PATH="/opt/llama/lib:$LD_LIBRARY_PATH"
+    export PATH="/opt/llama/bin:$PATH"
+elif [[ "$hostname" == "tux" ]]; then
+    source /opt/oneapi/setvars.sh
+    export LD_LIBRARY_PATH="/opt/llama/gpu/lib64:$LD_LIBRARY_PATH"
+    export PATH="/opt/llama/gpu/bin:$PATH"
+else
+    echo "Unknown hostname. Exiting."
+    exit 1
+fi
 exec llama-server --jinja -m /opt/llama/models/granite-3.3-2b-instruct-Q4_K_M.gguf --port 8080 -c 20000 --no-webui
