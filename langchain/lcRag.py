4480e9ce355a578600fafc3f18aced8c819c190c
Andreas Kugel <andreas.kugel@web.de>
2025-05-12 14:45:26 +0200

    langchain ollama modes. granite tool calls not reliably detected by langgraph

--- langchain/lcRag.py
+++ langchain/lcRag.py
@@ -4,8 +4,8 @@ import os
 os.environ["LANGCHAIN_TRACING_V2"] = "false"
 os.environ["LANGSMITH_TRACING"] = "false"
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../rag')))
-import private_remote as pr 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../rag")))
+import private_remote as pr
 
 from langchain_core.messages import SystemMessage
 from langgraph.checkpoint.memory import MemorySaver
@@ -16,7 +16,7 @@ from langchain_core.tools import tool
 
 from typing import List, Optional
 
-PROVIDER = "openai" # "ollama"   or "openai" "local" # 
+PROVIDER = "ollama" # "openai"  # "ollama"   or "openai" "local" #
 
 DEBUG = False
 
@@ -24,59 +24,81 @@ DEBUG = False
 
 
 if PROVIDER == "deepinfra":
-    from langgraph.prebuilt import create_react_agent    
+    from langgraph.prebuilt import create_react_agent
     from langchain_community.chat_models import ChatDeepInfra
+
     # Setup DeepInfra LLM
-    llm = ChatDeepInfra(model="meta-llama/Llama-3.3-70B-Instruct-Turbo",deepinfra_api_token=pr.deepInfra["apiKey"])
+    llm = ChatDeepInfra(
+        model="meta-llama/Llama-3.3-70B-Instruct-Turbo",
+        deepinfra_api_token=pr.deepInfra["apiKey"],
+    )
     llm_with_tools = None
 elif PROVIDER == "local":
     import localChatModel as LC
-    llm = LC.ChatLocal(model="granite-3.3-2b-instruct-Q4_K_M") #parrot_buffer_length=3, model="my_custom_model")
-    print("Using local model",llm)
+
+    llm = LC.ChatLocal(
+        model="granite-3.3-2b-instruct-Q4_K_M"
+    )  # parrot_buffer_length=3, model="my_custom_model")
+    print("Using local model", llm)
     llm_with_tools = None
 elif PROVIDER == "ollama":
     from langchain.chat_models import init_chat_model
     from langchain_community.chat_models import ChatLlamaCpp
+
     # https://python.langchain.com/api_reference/langchain/chat_models/langchain.chat_models.base.init_chat_model.html
     # chat providers limited. openAi work. deepinfra not supported here. huggingface complicated or not working
     # if ollama runs on remote machine, use ssh tunnel like:
     # ssh -L 8085:localhost:11434 user@remote
-    models = ["granite3.3:2b","llama3.2:latest"] # on pycontabo. small deeepseek and phi don't support tools
-    llm = init_chat_model(models[0], 
-                          model_provider="ollama",
-                          base_url="http://localhost:8085",
-                          temperature=0.1,
-                          max_tokens=10000,
-                          )
-    
+    # seems like granite3.3 tool request not always recognized by langgraph... message like:
+    # <|tool_call|>[{"arguments": {"query": "main feature of docling"}}]
+    # appears in output
+    # llama3.2  works normally
+    models = [
+        "granite3.3:2b",
+        "granite3.3:8b",
+        "granite3.1-moe:3b",
+        "llama3.2:latest"
+    ]  # on pycontabo. small deeepseek and phi don't support tools
+    os.environ["OPENAI_API_KEY"] = "ollama"
+    llm = init_chat_model(
+        models[3],
+        model_provider="ollama",
+        base_url="http://localhost:8085/",
+        temperature=0.1,
+        max_tokens=10000,
+    )
+
 else:
     from langchain.chat_models import init_chat_model
+
     os.environ["OPENAI_API_KEY"] = pr.openAi["apiKey"]
     # chat providers limited. openAi work. deepinfra not supported here. huggingface complicated or not working
     llm = init_chat_model("gpt-4o-mini", model_provider="openai")
 
 from langchain_community.embeddings import DeepInfraEmbeddings
-embMdl = "BAAI/bge-m3"
-embedder = DeepInfraEmbeddings(model_id=embMdl, deepinfra_api_token=pr.deepInfra["apiKey"])
 
-from langchain_community.vectorstores import FAISS
-# Load FAISS vector store
-vector_store = FAISS.load_local(
-    "test", embedder, allow_dangerous_deserialization=True
+embMdl = "BAAI/bge-m3"
+embedder = DeepInfraEmbeddings(
+    model_id=embMdl, deepinfra_api_token=pr.deepInfra["apiKey"]
 )
 
+from langchain_community.vectorstores import FAISS
+
+# Load FAISS vector store
+vector_store = FAISS.load_local("test", embedder, allow_dangerous_deserialization=True)
 
 
 @tool(response_format="content_and_artifact")
 def retrieve(query: str):
     """Retrieve information related to a query."""
+    if DEBUG:
+        print(f"Retrieving for query: {query}")
     retrieved_docs = vector_store.similarity_search(query, k=5, threshold=0.5)
     serialized = "\n\n".join(
         (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
         for doc in retrieved_docs
     )
     return serialized, retrieved_docs
-
 
 
 # Step 1: Generate an AIMessage that may include a tool-call to be sent.
@@ -93,9 +115,9 @@ def query_or_respond(state: MessagesState):
     return {"messages": [response]}
 
 
-
 # Step 2: Execute the retrieval.
 tools = ToolNode([retrieve])
+
 
 # Step 3: Generate a response using the retrieved content.
 def generate(state: MessagesState):
@@ -106,7 +128,7 @@ def generate(state: MessagesState):
         if message.type == "tool":
             recent_tool_messages.append(message)
             # check artifacts
-            if  message.artifact:
+            if message.artifact:
                 # print("Artifacts:", message.artifact)
                 doc_ids = [doc.metadata["doc_id"] for doc in message.artifact]
                 print("Docs:", doc_ids)
@@ -140,12 +162,12 @@ def generate(state: MessagesState):
 
 # Build graph
 
-#graph_builder = StateGraph(MessagesState)
+# graph_builder = StateGraph(MessagesState)
 graph_builder = StateGraph(MessagesState)
 
-graph_builder.add_node("query_or_respond",query_or_respond)
-graph_builder.add_node("tools",tools)
-graph_builder.add_node("generate",generate)
+graph_builder.add_node("query_or_respond", query_or_respond)
+graph_builder.add_node("tools", tools)
+graph_builder.add_node("generate", generate)
 
 graph_builder.set_entry_point("query_or_respond")
 graph_builder.add_conditional_edges(
@@ -160,15 +182,15 @@ memory = MemorySaver()
 graph = graph_builder.compile(checkpointer=memory)
 
 
-#with open("graph1.png","wb") as f:
+# with open("graph1.png","wb") as f:
 #    f.write(graph.get_graph().draw_mermaid_png())
-#print("Graph image written: graph1.png")
-#print(graph.get_graph())
+# print("Graph image written: graph1.png")
+# print(graph.get_graph())
 
 # Specify an ID for the thread
 config = {"configurable": {"thread_id": "abc123"}}
 
-###### 
+######
 input_message = "Hello"
 
 for step in graph.stream(
@@ -176,9 +198,10 @@ for step in graph.stream(
     stream_mode="values",
     config=config,
 ):
-    if DEBUG: step["messages"][-1].pretty_print()
-    if step["messages"][-1].type == "ai": # and not message.tool_calls:
-        print(step["messages"][-1].content)
+    if DEBUG:
+        step["messages"][-1].pretty_print()
+    if step["messages"][-1].type == "ai":  # and not message.tool_calls:
+        print("AI:",step["messages"][-1].content)
 ######
 
 input_message = "What is main feature of docling?"
@@ -188,12 +211,13 @@ for step in graph.stream(
     stream_mode="values",
     config=config,
 ):
-    if DEBUG: step["messages"][-1].pretty_print()
-    if step["messages"][-1].type == "ai": # and not message.tool_calls:
-        print(step["messages"][-1].content)
+    if DEBUG:
+        step["messages"][-1].pretty_print()
+    if step["messages"][-1].type == "ai":  # and not message.tool_calls:
+        print("AI:",step["messages"][-1].content)
 
 
-#config["configurable"]["thread_id"] = "abc1234"
+# config["configurable"]["thread_id"] = "abc1234"
 
 ######
 input_message = "Can docling do ocr?"
@@ -203,9 +227,10 @@ for step in graph.stream(
     stream_mode="values",
     config=config,
 ):
-    if DEBUG: step["messages"][-1].pretty_print()
-    if step["messages"][-1].type == "ai": # and not message.tool_calls:
-        print(step["messages"][-1].content)
+    if DEBUG:
+        step["messages"][-1].pretty_print()
+    if step["messages"][-1].type == "ai":  # and not message.tool_calls:
+        print("AI:",step["messages"][-1].content)
 
 ########
 
@@ -216,9 +241,10 @@ for step in graph.stream(
     stream_mode="values",
     config=config,
 ):
-    if DEBUG: step["messages"][-1].pretty_print()
-    if step["messages"][-1].type == "ai": # and not message.tool_calls:
-        print(step["messages"][-1].content)
+    if DEBUG:
+        step["messages"][-1].pretty_print()
+    if step["messages"][-1].type == "ai":  # and not message.tool_calls:
+        print("AI:",step["messages"][-1].content)
 
 ########
 
@@ -229,9 +255,10 @@ for step in graph.stream(
     stream_mode="values",
     config=config,
 ):
-    if DEBUG: step["messages"][-1].pretty_print()
-    if step["messages"][-1].type == "ai": # and not message.tool_calls:
-        print(step["messages"][-1].content)
+    if DEBUG:
+        step["messages"][-1].pretty_print()
+    if step["messages"][-1].type == "ai":  # and not message.tool_calls:
+        print("AI:",step["messages"][-1].content)
 
 ########
 
